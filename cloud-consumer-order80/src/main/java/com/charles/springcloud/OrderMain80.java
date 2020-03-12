63415d586ca6ba50f7d082fac95244b4d0690972
charles951008 <charles951008@163.com>
2020-03-12 19:32:53 +0800

    solve the Eureka configuration problems

--- cloud-consumer-order80/src/main/java/com/charles/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/charles/springcloud/OrderMain80.java
@@ -2,11 +2,13 @@ package com.charles.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author Charles
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class,args);
--- cloud-provider-payment8001/src/main/java/com/charles/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/charles/springcloud/PaymentMain8001.java
@@ -2,11 +2,14 @@ package com.charles.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author Charles
  */
+
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class,args);
