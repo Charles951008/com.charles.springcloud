590fdd7a7ab70778e01ac9903a83ea90fec4bce0
CharlesWSL <48273151+Charles951008@users.noreply.github.com>
2020-03-14 20:24:44 +0800

    choose different Load Balancing Algorithm to balance the service choosing depend on different situation

---
+++ cloud-consumer-order80/src/main/java/com/charles/myrule/MySelfRule.java
@@ -0,0 +1,24 @@
+package com.charles.myrule;
+
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author Charles
+ */
+@Configuration
+public class MySelfRule {
+    @Bean
+    @LoadBalanced
+
+    /**随机分发服务*/
+    public IRule getRestTemplate() {
+
+        return new RandomRule();
+
+    }
+}
--- cloud-consumer-order80/src/main/java/com/charles/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/charles/springcloud/OrderMain80.java
@@ -1,14 +1,18 @@
 package com.charles.springcloud;
 
+import com.charles.myrule.MySelfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 /**
+ * @description ribbon是consumer端进行协调选择服务（即负载均衡）的中间件 可以定为轮询 随机等等不同类型的负载均衡算法
  * @author Charles
  */
 @SpringBootApplication
 @EnableEurekaClient
+@RibbonClient(name="CLOUD-PAYMENT-SERVICE",configuration = MySelfRule.class)
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class,args);
